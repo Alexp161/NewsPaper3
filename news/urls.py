7f772a950f9c9f4a02bc7b7486e7921ecd60abfe
Alexp161 <116867471+Alexp161@users.noreply.github.com>
2023-06-03 08:25:13 +0300

    New Commit

--- news/urls.py
+++ news/urls.py
@@ -6,10 +6,14 @@ from django.urls import path
 from .views import ArticleListView
 from .views import ContactView
 from .views import ArticleCreateView
+from . import views
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
     path('contact/', ContactView.as_view(), name='contact'),
     path('add_article/', ArticleCreateView.as_view(), name='add_article'),
+    path('news/search', views.search, name='search'),
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='update_article'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='delete_article'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,29 +1,58 @@
-
-
-
-from django.views.generic import DetailView
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView, FormView, CreateView, UpdateView, DeleteView
 from .models import Article
-from django.views.generic.edit import FormView
-from .forms import ContactForm
-from django.views.generic.edit import CreateView
-from .forms import ArticleForm
+from .forms import ContactForm, ArticleForm
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.urls import reverse_lazy
 
 class ArticleCreateView(CreateView):
+    model = Article
     form_class = ArticleForm
-    template_name = 'news/add_article.html'  # пример имени шаблона
-    success_url = '/'  # URL для перенаправления после успешного добавления статьи
+    template_name = 'news/add_article.html'
+    success_url = reverse_lazy('article_list')
 
 class ContactView(FormView):
     form_class = ContactForm
-    template_name = 'news/contact.html'  # пример имени шаблона
-    success_url = '/'  # URL для перенаправления после успешной отправки формы
+    template_name = 'news/contact.html'
+    success_url = '/'
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'news/article_list.html'  # пример имени шаблона
-    ordering = ['-date']  # порядок от более свежих до старых
+    template_name = 'news/article_list.html'
+    ordering = ['-datetime_create']
 
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'news/article_detail.html'  # пример имени шаблона
+    template_name = 'news/article_detail.html'
+
+def search(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        date = request.POST.get('date')
+        # Используйте title, author и date для поиска новостей
+        # затем передайте результаты в контекст для отображения в шаблоне
+    else:
+        # Если метод не POST, отображаем пустую форму
+        return render(request, 'search.html', {})
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'update_article.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'delete_article.html'
+    success_url = reverse_lazy('article_list')
+
+def post_list_view(request):
+    post_list = Post.objects.all()
+
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'postList.html', {'page_obj': page_obj})
