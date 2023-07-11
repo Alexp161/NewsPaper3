76c837c9c62a2b75ae6a7d2c55fc5af2cca0a6d0
Alexp161 <116867471+Alexp161@users.noreply.github.com>
2023-07-12 00:46:15 +0300

    New Commit

---
+++ NewsPaper/exception_middleware.py
@@ -0,0 +1,11 @@
+class ExceptionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        try:
+            response = self.get_response(request)
+        except Exception as e:
+            logger.error('An error occurred: %s', e)
+            raise e
+        return response
